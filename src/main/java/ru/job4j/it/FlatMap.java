d063dec989e50852b0b6b82ba496332964000c00
Alexlife9 <crashmx@inbox.ru>
2021-09-24 00:57:50 +0300

    5.1.4. FlatMap для Iterator<Iterator>

---
+++ src/main/java/ru/job4j/it/FlatMap.java
@@ -0,0 +1,63 @@
+package ru.job4j.it;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Collections;
+
+/**
+ * Класс FlatMap принимает объект вложенных итераторов.
+ * @param <T>
+ * @author Alex_life
+ * @version 2.0
+ */
+public class FlatMap<T> implements Iterator<T> {
+    private final Iterator<Iterator<T>> data;
+    private Iterator<T> cursor = Collections.emptyIterator();
+
+    public FlatMap(Iterator<Iterator<T>> data) {
+        this.data = data;
+    }
+
+    private void check() {
+        if (cursor == null && data.hasNext()) {
+            cursor = data.next();
+        }
+    }
+
+    @Override
+    public boolean hasNext() {
+        check();
+        if (cursor.hasNext()) {
+            return true;
+        }
+        if (data.hasNext()) {
+            cursor = data.next();
+        }
+        return cursor.hasNext();
+    }
+
+    /**
+     * @return метод next должен последовательно вернуть числа из вложенных итераторов
+     */
+    @Override
+    public T next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        check();
+        return cursor.next();
+    }
+
+    public static void main(String[] args) {
+        Iterator<Iterator<Integer>> data = List.of(
+                List.of(1, 2, 3).iterator(),
+                List.of(4, 5, 6).iterator(),
+                List.of(7, 8, 9).iterator()
+        ).iterator();
+        FlatMap<Integer> flat = new FlatMap<>(data);
+        while (flat.hasNext()) {
+            System.out.println(flat.next());
+        }
+    }
+}
