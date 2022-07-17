f6e9961394de8359c9ce9a9c78142ddc8f958340
Alexlife9 <crashmx@inbox.ru>
2022-07-18 02:49:13 +0300

    Реализовать собственную структуру данных - HashMap

--- src/main/java/ru/job4j/map/SimpleMap.java
+++ src/main/java/ru/job4j/map/SimpleMap.java
@@ -8,8 +8,8 @@ import java.util.NoSuchElementException;
  * Реализация собственной структуры данных - HashMap
  *
  * @author Alex_life
- * @version 3.0
- * @since 17.07.2022
+ * @version 4.0
+ * @since 18.07.2022
  */
 public class SimpleMap<K, V> implements Map<K, V> {
 
@@ -17,7 +17,6 @@ public class SimpleMap<K, V> implements Map<K, V> {
      * LOAD_FACTOR - коэффициент загрузки хеш-таблицы
      * capacity - емкость хеш-таблицы
      * size - кол-во пар "ключ-значение" в хеш-таблице
-     * threshold - предельное количество элементов, при достижении которого размер хэш-таблицы увеличивается вдвое
      * modCount - счетчик изменений в хеш-таблице
      */
     private static final float LOAD_FACTOR = 0.75f;
@@ -35,7 +34,12 @@ public class SimpleMap<K, V> implements Map<K, V> {
     private MapEntry<K, V>[] table = new MapEntry[capacity];
 
     /**
-     * метод put вставляет новые элементы в хеш-таблицу. В случае отсутствия места возвращает false.
+     * метод put вставляет новые элементы в хеш-таблицу. В случае занятого бакета места возвращает false.
+     * 1.проверяем нужно ли увеличить емкость таблицы
+     * 2.если бакет куда мы хотим вставить элемент равен null, то значит он свободен и можно вставлять
+     * 3.увеличиваем счетчик элементов и счетчик кол-ва изменений
+     * 4.обязательно высвращаем успех операции
+     * 5.иначе false
      *
      * @param key - ключ элемента
      * @param value - значение элемента
@@ -48,6 +52,7 @@ public class SimpleMap<K, V> implements Map<K, V> {
             table[iB(key)] = new MapEntry<>(key, value);
             size++;
             modCount++;
+            return true;
         }
         return false;
     }
@@ -55,7 +60,7 @@ public class SimpleMap<K, V> implements Map<K, V> {
     /**
      * метод hash вычисляет хэш ключа бакета
      * @param hashCode на вход подаем хэшкод уже вычисленный внутренней реализацией
-     * @return хэш
+     * @return хэш реализованный нашим собственным способом. хэш-формула может быть разная
      */
     private int hash(int hashCode) {
         return hashCode ^ hashCode >>> 16;
@@ -72,16 +77,24 @@ public class SimpleMap<K, V> implements Map<K, V> {
 
     /**
      * индекс бакета с искомым ключом
+     * проверяем ключ на null (потому что нельзя использовать в качестве ключа null в хэш-таблицах)
      * @param key ключ
-     * @return индекс
+     * @return вычесленный индекс для бакета либо просто последний индекс таблицы (если ключ равен null)
      */
     public int iB(K key) {
-        return indexFor(hash(key.hashCode()));
+        if (key != null) {
+            return indexFor(hash(key.hashCode()));
+        }
+        return capacity - 1;
     }
 
     /**
      * метод expand увеличивает capacity вдвое
-     * если кол-во объектов больше или равно предельному значению капасити, то увеличиваем емкость вдвое
+     * 1.если кол-во объектов больше или равно предельному значению капасити, то увеличиваем емкость таблицы вдвое
+     * 2.создаем таблицу tableNew с текущей емкостью
+     * 3.для каждого элемента в старой таблице (если элемент не равен null)
+     * вычислем индекс нового бакета и кладем его на новое место
+     * 4.после того как прошли все элементы сохраняем новую таблицу под старым именем table
      */
     private void expand() {
         if ((float) size / table.length > LOAD_FACTOR) {
@@ -98,8 +111,11 @@ public class SimpleMap<K, V> implements Map<K, V> {
 
     /**
      * метод get для доступа к значению элемента по ключу
+     * 1.если бакет куда мы хотим вставить элемент НЕ равен null И ключ этого бакета эквивалентен искомому ключю,
+     * 2.то возвращаем значение этого найденного элемента в найденном бакете
+     * 3.если не нашли - возвращаем null
      * @param key ключ ноды
-     * @return возвращает найденное значение
+     * @return возвращает найденное значение либо null если не нашли
      */
     @Override
     public V get(K key) {
@@ -111,6 +127,9 @@ public class SimpleMap<K, V> implements Map<K, V> {
 
     /**
      * метод remove удаляет ноду по ключу
+     * рабоатет аналогично методу get
+     * только вместо возврата значения мы обнуляем найденный бакет
+     * и уменьшаем счетчик элементов в случае удачи и увеличиваем счетчик изменений
      * @param key ключ
      * @return true если успешно
      */
@@ -135,7 +154,7 @@ public class SimpleMap<K, V> implements Map<K, V> {
              * Это называется fail-fast поведением
              *
              * увеличиваем счетчик указателя по бакетам, пока они равны null И пока есть след-й элемент
-             * возвращаем true если счетчик указателя меньше кол-ва непустых бакетов, значит след-й элемент есть
+             * возвращаем true если счетчик указателя меньше емкости таблицы, значит след-й элемент есть
              */
             private int cursor = 0;
             private final int expectedModCount = modCount;
@@ -147,7 +166,7 @@ public class SimpleMap<K, V> implements Map<K, V> {
                 while (table.length != cursor && table[cursor] == null) {
                     cursor++;
                 }
-                return cursor < table.length;
+                return cursor < capacity - 1;
             }
 
             /**
