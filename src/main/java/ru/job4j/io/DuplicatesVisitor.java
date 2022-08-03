30876afa683a9faec075fa1a3a3a07187cf94894
Alexlife9 <crashmx@inbox.ru>
2022-08-03 16:50:24 +0300

    Поиск дубликатов

--- src/main/java/ru/job4j/io/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/DuplicatesVisitor.java
@@ -5,8 +5,8 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Поиск дубликатов
@@ -20,13 +20,20 @@ import java.util.List;
  * Например, для выхода всех файлов в консоль мы можем написать такой короткий код: в классе DuplicatesVisitor
  *
  * @author Alex_life
- * @version 1.0
+ * @version 3.0
  * @since 03.08.2022
  */
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
 
+    Set<FileProperty> duplicateFiles = new HashSet<>();
     /**
      * в методе visitFile реализована логика поиска дубликатов
+     * метод visitFile обходит весь путь от указанного старта до корней папок
+     *
+     * 1.В коллекции Set невозможно содержать дубли
+     * 2.поэтому создаем множество duplicateFiles
+     * 3.и проверяем - если элемент не смогли добавить, значит он уже есть в коллекции, значит он дубль
+     * 4.выводим имя файла-дубликата в консоль
      *
      * @param file - текущий файл
      * @param attrs - атрибуты файла (оставляем все по-умолчанию)
@@ -35,13 +42,9 @@ public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
      */
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        List<FileProperty> duplicateFiles = new ArrayList<>();
-        List<FileProperty> allFiles = new ArrayList<>();
         FileProperty fileProperty = new FileProperty(file.toFile().length(), file.getFileName().toString());
-        duplicateFiles.add(fileProperty);
-        allFiles.add(fileProperty);
-        if (duplicateFiles.size() != allFiles.size()) {
-            System.out.println("найден дубль : " + "и тут надо понять как вывести этот дубль :)");
+        if (!duplicateFiles.add(fileProperty)) {
+            System.out.println("не смогли добавить файл в Сет, значит это дубль: " + fileProperty.getName());
         }
         return super.visitFile(file, attrs);
     }
