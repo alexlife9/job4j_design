e52fee1ac58734392e98fcc5cf88c25824bd7f08
Alexlife9 <crashmx@inbox.ru>
2022-08-03 16:29:45 +0300

    Поиск дубликатов

--- src/main/java/ru/job4j/io/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/DuplicatesVisitor.java
@@ -5,8 +5,12 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
+ * Поиск дубликатов
+ *
  * SimpleFileVisitor для быстрого обхода директории без переопределения всех методов FileVisitor
  *
  * SimpleFileVisitor уже реализует FileVisitor,
@@ -20,9 +24,25 @@ import java.nio.file.attribute.BasicFileAttributes;
  * @since 03.08.2022
  */
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
+
+    /**
+     * в методе visitFile реализована логика поиска дубликатов
+     *
+     * @param file - текущий файл
+     * @param attrs - атрибуты файла (оставляем все по-умолчанию)
+     * @return возвращаем результат работы метода - то есть все найденные дубликаты
+     * @throws IOException выбрасываем эксепшн если файл не найден
+     */
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        System.out.println(file.toAbsolutePath());
+        List<FileProperty> duplicateFiles = new ArrayList<>();
+        List<FileProperty> allFiles = new ArrayList<>();
+        FileProperty fileProperty = new FileProperty(file.toFile().length(), file.getFileName().toString());
+        duplicateFiles.add(fileProperty);
+        allFiles.add(fileProperty);
+        if (duplicateFiles.size() != allFiles.size()) {
+            System.out.println("найден дубль : " + "и тут надо понять как вывести этот дубль :)");
+        }
         return super.visitFile(file, attrs);
     }
 }
