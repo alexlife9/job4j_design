342940bd78b36de368779deaa8020997bf53e58f
Alexlife9 <crashmx@inbox.ru>
2022-08-04 15:33:26 +0300

    Архивировать проект

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -16,7 +16,7 @@ import java.util.zip.ZipOutputStream;
  * 2. В качестве ключа передаётся расширение файлов, которые не нужно включать в архив.
  * 3. Архив должен сохранять структуру проекта. То есть содержать подпапки.
  * 4. Запуск проекта с помощью конфигурации параметров запуска:
- * java -jar pack.jar -d=c:\project\job4j\ -e=.class -o=project.zip
+ * java -jar pack.jar -d=c:\project\job4j_design -e=*.java -o=project.zip
  * где:
  * java -jar pack.jar - это собранный jar.
  * -d - directory - которую мы хотим архивировать.
@@ -29,7 +29,7 @@ import java.util.zip.ZipOutputStream;
  * 7. Для поиска и фильтрации файлов используем класс Search.
  *
  * @author Alex_life
- * @version 1.0
+ * @version 2.0
  * @since 04.08.2022
  */
 public class Zip {
@@ -48,22 +48,29 @@ public class Zip {
             e.printStackTrace();
         }
     }
-
-    public static void main(String[] args) throws IOException {
-        String directory = "d";
-        String exclude = "e";
-        String output = "o";
-        if (args.length != 3) {
+    private static void checkArgs(String[] chArg) {
+        ArgsName argsName = ArgsName.of(chArg);
+        if (chArg.length != 3) {
             throw new IllegalArgumentException("Указаны не все аргументы для запуска");
         }
-        if (!exclude.startsWith(".")) {
-            throw new IllegalArgumentException("Не указано расширение файла или расширение не начинается с '.'");
+        if (!Path.of(argsName.get("d")).toFile().isDirectory()) {
+            throw new IllegalArgumentException("Указана некорректная директория");
         }
-        ArgsName argsName = ArgsName.of(args);
-        List<Path> searchFiles = Search.search(Paths.get(argsName.get(directory)),
-                path -> path.toFile().getName().endsWith(output));
-        new Zip().packFiles(searchFiles, new File(argsName.get(output)));
+        if (!argsName.get("e").startsWith("*.")) {
+            throw new IllegalArgumentException("Указано некорректное расширение файла");
+        }
+        if (!argsName.get("o").endsWith(".zip")) {
+            throw new IllegalArgumentException("Указано некорректный тип файла для архивирования");
+        }
 
 
+    }
+
+    public static void main(String[] args) throws IOException {
+        checkArgs(args);
+        ArgsName argsN = ArgsName.of(args);
+        List<Path> searchFiles = Search.search(Paths.get(argsN.get(args[0])),
+                path -> path.toFile().getName().endsWith(args[2]));
+        new Zip().packFiles(searchFiles, new File(argsN.get(args[2])));
     }
 }
