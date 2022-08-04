357dc969e7b15278b2ee6908b5452ba8f924bad5
Alexlife9 <crashmx@inbox.ru>
2022-08-04 21:21:52 +0300

    Именованные аргументы

--- src/main/java/ru/job4j/io/ArgsName.java
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -7,10 +7,11 @@ import java.util.Map;
  * Именованные аргументы
  *
  * Класс ArgsName принимает массив параметров и разбивает их на пары: ключ, значение.
+ * -Xmx=512 -encoding=UTF-8
  *
  * @author Alex_life
- * @version 6.0
- * вынес проверку аргументов в отдельный метод
+ * @version 7.0
+ * исправил логику проверки аргументов
  * @since 04.08.2022
  */
 public class ArgsName {
@@ -26,36 +27,34 @@ public class ArgsName {
 
     /**
      * метод parse принимает массив аргументов и разбивает их на пары "ключ-значение"
-     * 1.проверяем что аргументы вообще есть. если нет - выбрасываем ошибку
-     * 2.проходим циклом по всему массиву args
-     * 3.удаляем у первого аргумента первое вхождение "-" и сохраняем в elements
-     * 4.разбиваем элемент на пары по знаку "=" и сохраняем получившуюся пару "ключ-значение" в массив array
-     * 5.если в массиве 0 или 1 элемент ИЛИ первая или вторая ячейка пустая, то выбрасываем ошибку
-     * 6.остальные пары добавляем в хэшмапу в виде готового бакета
-     * 7.после этого проверяем второй аргумент и повторяем пока они не кончатся
+     * 1.проходим циклом по всему массиву args
+     * 2.удаляем (если оно есть) у первого аргумента первое вхождение "-", разбиваем элемент на пары по знаку "=".
+     * 3.запускаем проверку текущего аргумента на корректность переданных параметров
+     * 4.сохраняем получившуюся пару "ключ-значение" в хэшмапу в виде готового бакета
+     * 8.после этого возвращаемся в цикл и проверяем второй аргумент и повторяем это пока они не кончатся
      * @param args входящие аргументы
      */
     private void parse(String[] args) {
         for (String arguments : args) {
-            String[] array = arguments.split("=", 2);
-            argsCheck(array);
+            String[] array = arguments.replaceFirst("-", "").split("=", 2);
+            argsCheck(arguments);
             values.put(array[0], array[1]);
         }
     }
 
-    private void argsCheck(String[] argValid) {
-        if (!argValid[0].startsWith("-")) {
+    /**
+     * метод argsCheck проверяет корректность переданных параметров в аргументы
+     * @param argValid проверяемые аргументы
+     */
+    private void argsCheck(String argValid) {
+        if (!argValid.startsWith("-")) {
             throw new IllegalArgumentException("Неверное значение ключа");
         }
-
-        for (String s : argValid) {
-            if (s.isEmpty()) {
-                throw new IllegalArgumentException("Отсутствует ключ или значение");
-            }
-        }
-
-        if (!argValid[0].startsWith("-=")) {
+        if (argValid.startsWith("-=")) {
             throw new IllegalArgumentException("Отсутствует ключ");
+        }
+        if (!argValid.contains("=")) {
+            throw new IllegalArgumentException("Отсутствует аргумент");
         }
 
     }
@@ -69,6 +68,14 @@ public class ArgsName {
         return names;
     }
 
+    /**
+     * метод main принимает входящий строковый массив аргументов
+     * аргументы правильно записываются строго по шаблону: -ключ=значение
+     * между аргументами ОДИН пробел
+     * если нет дефиса или знака равно, то это не аргумент
+     * если нет ключа между дефисом и равно, то это ошибочный аргумент
+     * @param args аргументы запуска
+     */
     public static void main(String[] args) {
         ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
         System.out.println(jvm.get("Xmx"));
