f96f7b7c30bf237112b98085871f3309dd7a1f64
Alexlife9 <crashmx@inbox.ru>
2022-08-06 21:34:35 +0300

    Формат JSON

---
+++ src/main/java/ru/job4j/serialization/Car.java
@@ -0,0 +1,68 @@
+package ru.job4j.serialization;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.Arrays;
+
+/**
+ * Формат JSON
+ *
+ * @author Alex_life
+ * @version 1.0
+ * @since 06.08.2022
+ */
+public class Car {
+    boolean refueled;
+
+    int releaseYear;
+
+    String color;
+
+    String[] trunk;
+
+    Driver driver;
+
+    public Car(boolean refueled, int releaseYear, String color, String[] trunk, Driver driver) {
+        this.refueled = refueled;
+        this.releaseYear = releaseYear;
+        this.color = color;
+        this.trunk = trunk;
+        this.driver = driver;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{"
+                + "refueled=" + refueled
+                + ", releaseYear=" + releaseYear
+                + ", color='" + color + '\''
+                + ", trunk=" + Arrays.toString(trunk)
+                + '}';
+    }
+
+    public static void main(String[] args) {
+        String[] trunk = {"Suitcase",  "Painting", "Basket", "Cardboard",  "smallDog"};
+        Driver driver = new Driver("Alex", 40);
+        Car car = new Car(true, 2000, "black", trunk, driver);
+
+        final Gson gson = new GsonBuilder().create();
+        System.out.println(gson.toJson(car));
+
+        final String carJson = gson.toJson(car);
+        final Car carMod = gson.fromJson(carJson, Car.class);
+        System.out.println(carMod);
+
+
+    }
+}
+
+class Driver {
+    String name;
+    int age;
+
+    public Driver(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
