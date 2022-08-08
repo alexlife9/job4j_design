6b35c046a3183f1b500ea3ac9e3c6ca6d2b0b82d
Alexlife9 <crashmx@inbox.ru>
2022-08-09 02:14:27 +0300

    Scanner

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -7,7 +7,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Scanner
@@ -16,8 +18,8 @@ import java.util.List;
  *
  *
  * @author Alex_life
- * @version 2.0
- * @since 06.08.2022
+ * @version 3.0
+ * @since 09.08.2022
  */
 public class CSVReader {
 
@@ -25,8 +27,10 @@ public class CSVReader {
 
     private static String outputInfo;
 
+    static List<String> afterFilter = new ArrayList<>(); /*лист токенов после разбивки по разделителю*/
+
     /**
-     * метод handle принимает аргументы, читает фходящий файл из csv и применяет фильтр
+     * метод handle принимает аргументы, читает фходящий файл из csv и делит его на ткены по разделителю
      *
      * java -jar target/csvReader.jar -path=file.csv -delimiter=";"  -out=stdout -filter=name,age
      * @param argsName - аргументы для запуска
@@ -37,17 +41,33 @@ public class CSVReader {
         String filter = argsName.get("filter");
         String out = argsName.get("out");
         checkArgs(path, delimiter, filter, out);
-        List<String> outputInfo = new ArrayList<>();
+
         try (BufferedReader inputData = new BufferedReader(new FileReader(String.valueOf(path)))) {
-            outputInfo.add(String.valueOf(inputData));
-
-
-            outputData(outputInfo);
+            Scanner scanner = new Scanner(String.valueOf(inputData)); /*читаем сканером входящий файл*/
+            String lines = inputData.readLine();
+            while (lines != null) { /*пока есть строки*/
+                scanner.useDelimiter(argsName.get(delimiter)); /*применяем разбивку на токены по разделителю*/
+                afterFilter.add(lines); /*добавляем разбитые токены из входящего файла в лист строк*/
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * после того как прочитали файл и разбили его на токены, применяем фильтр и вычленяем необходимые нам данные
+     * метод headLine формирует шапку файла исходя из заданного фильтра, переданного в аргументах
+     */
+    public static List<Integer> headLine(ArgsName argsName, String column) {
+        int[] table; /*массив в который будем записывать отфильтрованные данные под порядковыми номерами*/
+        List<String> firstLine = Arrays.asList(argsName.get("filter").split(","));
+        while (afterFilter != null) {
+            if (firstLine.contains("out")) {
+
+            }
+        }
+
+    }
 
     /**
      * метод outputData выбирает куда выводить отфильтрованные данные - в консоль или в файл
@@ -98,6 +118,7 @@ public class CSVReader {
      */
     public static void main(String[] args) throws Exception {
         ArgsName argsNames = ArgsName.of(args);
+        outputData(..........);
         handle(argsNames);
 
 
