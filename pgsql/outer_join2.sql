bb4ce8fe174250990c65d93130d0e053288eebe5
Alexlife9 <crashmx@inbox.ru>
2022-08-18 01:31:03 +0300

    Join

--- pgsql/outer_join2.sql
+++ pgsql/outer_join2.sql
@@ -28,19 +28,19 @@ insert into employees(name, departments_id) values (null, 5);
 /*2. Выполнить запросы с left, rigth, full, cross соединениями */
 select *
 from employees AS e
-left join departments AS d ON e.departments_id = d.id
+left join departments AS d ON e.departments_id = d.id;
 
 select *
 from employees AS e
-right join departments AS d ON e.departments_id = d.id
+right join departments AS d ON e.departments_id = d.id;
 
 select *
 from employees AS e
-full join departments AS d ON e.departments_id = d.id
+full join departments AS d ON e.departments_id = d.id;
 
 select *
 from employees AS e
-cross join departments AS d
+cross join departments AS d;
 
 /* 3. Используя left join найти департаменты, у которых нет работников */
 insert into departments(name) values ('калыма');
@@ -55,13 +55,18 @@ group by d.id;
 /*4. Используя left и right join написать запросы, 
 которые давали бы одинаковый результат (порядок вывода колонок в эти запросах также должен быть идентичный). */
 
-select *
-from employees AS e
-left join departments AS d ON e.departments_id = d.id
 
-select *
+select d.name AS департ
 from departments AS d
-right join employees AS e ON e.departments_id = d.id
+left join employees AS e ON d.id = e.departments_id
+where e.departments_id is not null
+group by d.id;
+
+select d.name AS департ
+from employees AS e
+right join departments AS d ON d.id = e.departments_id
+where e.departments_id is not null
+group by d.id;
 
 /*5. Создать таблицу teens с атрибутами name, gender и заполнить ее. Используя cross join составить все возможные разнополые пары */
 create table teens(
